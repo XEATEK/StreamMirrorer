be97f3a82a660a2b89e3ee1d4b75bb7335d186a7
XEATAK <114109407+XEATEK@users.noreply.github.com>
2025-10-02 22:47:14 +0200

    DeppInject

--- Services/RecordController.cs
+++ Services/RecordController.cs
@@ -1,8 +1,15 @@
 using Recorder.Interfaces;
+using Recorder.Recorders;
 
 namespace Recorder.Services;
 
 public class RecordController : IRecordController
 {
-    
+
+    TwitchRecorder _recorder { get; }
+
+    public RecordController(TwitchRecorder recorder)
+    {
+        _recorder = recorder;
+    }
 }
